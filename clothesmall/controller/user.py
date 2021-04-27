870e3c4a02c0c59b6984f5a6275d6af4fac46382
mae <myeongheeroh@gmail.com>
2021-04-27 18:14:02 +0900

    Add user register email duplicate check

--- clothesmall/controller/user.py
+++ clothesmall/controller/user.py
@@ -1,5 +1,5 @@
 import os
-from flask import render_template, request, session, Flask, g, redirect, flash
+from flask import render_template, request, session, Flask, g, redirect, flash, jsonify
 from database import DBManager
 from model.user import User
 from flask import Blueprint
@@ -18,3 +18,24 @@ def get_db():
 @bp.route('/user/register')
 def user():
     return render_template('register.html')
+
+def __get_user(email):
+    try:
+        print('!!!!!!!!!!!!!!!!!!', email)
+        current_user = g.db.query(User).filter(User.email == email).first()
+        print('??????????????????', current_user)
+        return current_user
+    except Exception as e:
+        print('__get_user_error', str(e))
+        raise e
+
+@bp.route('/user/check_email', methods=['POST'])
+def check_email():
+    print('--이메일 중복 체크--')
+    email = request.get_json()
+    print('이메일', email)
+    #DB에서 email 중복확인
+    if __get_user(email) == None :
+        return jsonify(result = False)
+    else:
+        return jsonify(result = True)
