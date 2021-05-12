0f6e56d86e0f87d48911bf0f1a06870ddd44c798
mae <myeongheeroh@gmail.com>
2021-05-12 16:15:04 +0900

    Add BaseLayoutModifier.vue and BasePageHeading.vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,22 +1,29 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
+import App from './App.vue'
+import store from './store'
+import { BootstrapVue , IconsPlugin } from 'bootstrap-vue'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import BaseLayoutModifier from '@/components/BaseLayoutModifier'
 import BaseBlock from '@/components/BaseBlock'
+import BasePageHeading from '@/components/BasePageHeading'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
+Vue.component(BaseLayoutModifier.name, BaseLayoutModifier)
+Vue.component(BasePageHeading.name, BasePageHeading)
 Vue.component(BaseBlock.name, BaseBlock)
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount('#app')
