a3c1b81d60ead0e25d944dc45709667921dce2b4
mae <myeongheeroh@gmail.com>
2021-05-12 17:15:49 +0900

    Add pages/boxed Boxed Backend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,17 +8,31 @@ import { BootstrapVue , IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// Custom components
 import BaseLayoutModifier from '@/components/BaseLayoutModifier'
 import BaseBlock from '@/components/BaseBlock'
+import BaseBackground from '@/components/BaseBackground'
 import BasePageHeading from '@/components/BasePageHeading'
+import BaseNavigation from '@/components/BaseNavigation'
+
+// Custom directives
+import clickRipple from '@/directives/clickRipple'
+import toggleClass from '@/directives/toggleClass'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
+// Register global components
 Vue.component(BaseLayoutModifier.name, BaseLayoutModifier)
-Vue.component(BasePageHeading.name, BasePageHeading)
 Vue.component(BaseBlock.name, BaseBlock)
+Vue.component(BaseBackground.name, BaseBackground)
+Vue.component(BasePageHeading.name, BasePageHeading)
+Vue.component(BaseNavigation.name, BaseNavigation)
+
+// Register global directives
+Vue.directive('click-ripple', clickRipple)
+Vue.directive('toggle-class', toggleClass)
 
 Vue.config.productionTip = false
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,83 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Ping from '../components/Ping.vue';
-import Simple from '../components/Simple.vue';
+import Ping from '../components/Ping'
+
+// Main layouts
+import LayoutBackend from '@/layouts/variations/Backend.vue'
+import LayoutBackendBoxed from '@/layouts/variations/BackendBoxed.vue'
+import LayoutSimple from '@/layouts/variations/Simple.vue'
 
 Vue.use(Router);
 
+// Frontend: Landing
+const Landing = () => import("@/views/Landing.vue")
+
+// Backend: General
+const Dashboard = () => import(/* webpackChunkName: "pages-dashboard", webpackPrefetch: true */"@/views/Dashboard.vue")
+
+// Pages: Boxed Backend
+const BoxedDashboard = () => import(/* webpackChunkName: "pages-boxed-dashboard" */"@/views/pages/boxed/Dashboard.vue")
+const BoxedSearch = () => import("@/views/pages/boxed/Search.vue")
+const BoxedSimple1 = () => import("@/views/pages/boxed/Simple1.vue")
+const BoxedSimple2 = () => import("@/views/pages/boxed/Simple2.vue")
+const BoxedImage1 = () => import("@/views/pages/boxed/Image1.vue")
+const BoxedImage2 = () => import("@/views/pages/boxed/Image2.vue")
+
+// Router Configuration
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
+  linkActiveClass: 'active',
+  linkExactActiveClass: '',
+  scrollBehavior() {
+    return { x: 0, y: 0 }
+  },
   routes: [
     {
-      path: '/ping',
-      name: 'Ping',
-      component: Ping,
+      path: '/',
+      component: LayoutSimple,
+      children: [
+        {
+          path: '/',
+          name: 'Home',
+          component: Landing
+        },
+      ]
     },
     {
-      path: '/',
-      name: 'Simple',
-      component: Simple,
+      path: '/backend-boxed',
+      redirect: '/backend-boxed/dashboard',
+      component: LayoutBackendBoxed,
+      children: [
+        {
+          path: 'dashboard',
+          name: 'Boxed Dashboard',
+          component: BoxedDashboard
+        },
+        {
+          path: 'search',
+          name: 'Boxed Search',
+          component: BoxedSearch
+        },
+        {
+          path: 'simple1',
+          name: 'Boxed Simple1',
+          component: BoxedSimple1
+        },
+        {
+          path: 'simple2',
+          name: 'Boxed Simple2',
+          component: BoxedSimple2
+        },
+        {
+          path: 'image1',
+          name: 'Boxed Image1',
+          component: BoxedImage1
+        },
+        {
+          path: 'image2',
+          name: 'Boxed Image2',
+          component: BoxedImage2
+        }
+      ]
     },
-  ],
+  ]
 });
