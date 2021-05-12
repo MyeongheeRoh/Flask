c967e35a468a9cbea944d392084d5fe7b7abd07d
mae <myeongheeroh@gmail.com>
2021-05-12 14:58:12 +0900

    Add scss and BaseBlock.vue, Simple Dashboard

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,25 @@
 <template>
-  <div id="app">
     <router-view/>
-  </div>
 </template>
+
+<style lang="scss">
+// Main Stylesheet
+@import './src/assets/scss/main';
+
+// Bootstrap Vue Stylesheet
+@import '~bootstrap-vue/src/index';
+
+// All color themes are included and available by default
+// Feel free to comment out any of them if you won't use them in your project
+@import './src/assets/scss/oneui/themes/amethyst';
+@import './src/assets/scss/oneui/themes/city';
+@import './src/assets/scss/oneui/themes/flat';
+@import './src/assets/scss/oneui/themes/modern';
+@import './src/assets/scss/oneui/themes/smooth';
+</style>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import BaseBlock from '@/components/BaseBlock'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+
+Vue.component(BaseBlock.name, BaseBlock)
 
 Vue.config.productionTip = false
 
